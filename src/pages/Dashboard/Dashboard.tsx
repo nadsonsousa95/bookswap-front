6483715470910473456c28fa53600e560aa2173e
nadsonsousa95 <nadsonbcs95@gmail.com>
2025-06-26 16:59:33 -0300

    feat: create Dashboard page

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,145 @@
+import { Header } from "../../components/Header/Header"
+import styles from './Dashboard.module.css'
+import { useState } from "react";
+import type { StudentProfile } from "../../types/ProfileProps";
+import profileImg from './../../assets/img/profile.svg'
+import { Link } from "react-router-dom";
+
 function Dashboard() {
+    const [isEditing, setIsEditing] = useState(false);
+    const [profile, setProfile] = useState<StudentProfile>({
+      name: "Nadson Benigno",
+      email: "nadson@email.com",
+      telephone: '71992232517',
+      course: "Ciência da Computação",
+      bio: "Apaixonado por tecnologia e educação.",
+      img: ''
+    });
+
+    const handleChange = (
+      e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+      ) => {
+      const { name, value } = e.target;
+      setProfile((prev) => ({ ...prev, [name]: value }));
+      };
+
+      const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>
+       ) => {
+        const file = e.target.files?.[0];
+        if (file) {
+          const imageUrl = URL.createObjectURL(file);
+          setProfile((prev) => ({ ...prev, img: imageUrl }));
+        }
+      };
+
+    const handleEditToggle = () => {
+      setIsEditing(!isEditing);
+      };
 
   return (
     <div>
-        <h1>Dashboard</h1>
+      <Header></Header>
+      <div className={styles.container}>
+      <h1 className={styles.title}>Perfil</h1>
+      <p className={styles.subtitle}>Gerencie suas informações e livros</p>
+
+      <div className={styles.profileCard}>
+        <label>
+          <strong>Foto de Perfil:</strong><br></br>
+          {isEditing ? (
+            <input 
+               type="file" 
+               accept="image/*" 
+               onChange={handleImageChange} 
+            />) : (
+                <img 
+                  className={styles.imgprofile}
+                  src={profile.img || profileImg}
+                  alt="Foto de Perfil"
+                  ></img>
+                )}
+          {isEditing && profile.img && (
+              <img 
+                src={profile.img} 
+                alt="Pré-visualização" 
+                className={styles.imgpreview}
+              />
+            )}
+
+        </label>
+        <label>
+          <strong>Nome:</strong>
+          {isEditing ? (
+            <input
+              className={styles.input} 
+              type="text" 
+              name="name" 
+              value={profile.name} 
+              onChange={handleChange} />
+          ) : (
+            <p>{profile.name}</p>
+          )}
+        </label>
+
+        <label>
+          <strong>Email:</strong>
+          {isEditing ? (
+            <input
+              className={styles.input} 
+              type="email" 
+              name="email" 
+              value={profile.email} 
+              onChange={handleChange} />
+          ) : (
+            <p>{profile.email}</p>
+          )}
+        </label>
+
+        <label>
+          <strong>Curso:</strong>
+          {isEditing ? (
+            <input
+              className={styles.input} 
+              type="text" 
+              name="course" 
+              value={profile.course} 
+              onChange={handleChange} />
+          ) : ( <p>{profile.course}</p> )}
+        </label>
+
+        <label>
+          <strong>Telefone: </strong>
+          {isEditing ? (
+            <input
+              className={styles.input} 
+              type="text" 
+              name="telephone" 
+              value={profile.telephone} 
+              onChange={handleChange} />
+          ) : ( <p>{profile.telephone}</p> )}
+        </label>
+
+        <label>
+          <strong>Bio:</strong>
+          {isEditing ? (
+            <textarea
+              className={styles.textarea} 
+              name="bio" 
+              value={profile.bio} 
+              onChange={handleChange} />
+          ) : ( <p>{profile.bio}</p> )}
+        </label>
+
+        <button className={styles.button} 
+          onClick={handleEditToggle}>
+          {isEditing ? "Salvar" : "Editar Perfil"}
+        </button>
+      </div>
+        <h2 className={styles.title}>Meus Livros</h2>
+        <Link to={'/'}>
+            <button className={styles.buttoNewBook}>Adicionar Novo Livro</button>
+        </Link>
+    </div>
     </div>
   )
 }
---
+++ src/types/ProfileProps.ts
@@ -0,0 +1,8 @@
+export interface StudentProfile {
+  name: string;
+  email: string;
+  telephone: string;
+  course?: string;
+  bio?: string;
+  img?: string | undefined;
+}
