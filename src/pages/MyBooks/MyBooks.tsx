b0ad4630022b373d3ac39e8b368a903cf9f49a1b
nadsonsousa95 <nadsonbcs95@gmail.com>
2025-07-10 00:24:57 -0300

    feat: create MyBooks page

--- src/pages/MyBooks/MyBooks.tsx
+++ src/pages/MyBooks/MyBooks.tsx
@@ -1,33 +1,48 @@
 import { PainelHeader } from "../../components/PainelHeader/PainelHeader"
 import { Header } from "../../components/Header/Header"
-//import { useAuth } from "../../contexts/AuthContext.tsx";
+import { useAuth } from "../../contexts/AuthContext.tsx";
 import { useState, useEffect } from "react";
 import type  {Book}  from "../../types/Book.ts";
 import { BooksList } from "../../components/BooksList/BookList.tsx";
 import styles from './MyBooks.module.css'
+import { getBooksByUserId } from "../../services/bookService.ts";
+import { Footer } from "../../components/Footer/Footer.tsx";
 
 export default function MyBooks(){
-    //const {user} = useAuth()
+    const {user} = useAuth()
     const [internalbooks, setInternalBooks] = useState<Book[]>([])
     const [loading, setLoading] = useState(true)
 
-    useEffect(()=>{
-
-    },[])
+    useEffect(()=> {
+    async function fetchBooks() {
+      if (!user) return;
+      try {
+        const data = await getBooksByUserId(String(user.id));
+        setInternalBooks(data);
+      } catch (err) {
+        console.error("Erro ao carregar livros:", err);
+      } finally {
+        setLoading(false);
+      }
+    }
+       fetchBooks();
+    },[user]);
 
     return(
         <div>
             <Header/>
             <div className={styles.container}>
                 <PainelHeader/>
-                <div>
+                <div className={styles.mybooks}>
                      <h2>Meus Livros Publicados</h2>
                     <div>
                         {loading && <p>Carregando livros...</p>}
+                        <BooksList books={internalbooks} />
                         </div>
-                    <BooksList books={internalbooks} />
+                    
                 </div>
             </div>
+            <Footer></Footer>
         </div>
     )
 }
--- src/services/bookService.ts
+++ src/services/bookService.ts
@@ -6,4 +6,10 @@ const BASE_URL = "http://localhost:3001/books";
 export const getBooks = () => axios.get(BASE_URL)
 export const getBookById = (id: string) => axios.get(`${BASE_URL}/${id}`)
 export const createBook = (book: Book) => axios.post(BASE_URL, book);
-export const deleteBook = (id: string) => axios.delete(`${BASE_URL}/${id}`)
+export const deleteBookById = (id: string) => axios.delete(`${BASE_URL}/${id}`)
+export const getBooksByUserId = async (userid: string) =>{
+     const res = await axios.get(BASE_URL, {
+      params: { userid } 
+    });
+    return res.data
+  }
