911cb0a561007a53bcb5ae726d402c855249c02a
nadsonsousa95 <nadsonbcs95@gmail.com>
2025-07-16 00:40:15 -0300

    feat: create service createUser in AuthService

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -13,6 +13,26 @@ export const AuthService = {
             }else{
                 throw new Error('Email ou senha inválidos')
             }
-    }
+    },
 
+    async createUser(name: string, email: string, password:string){
+        try{
+            const res = await axios.post(BASE_URL, {
+                name,
+                email,
+                password
+            });
+
+            return res.data;
+        }catch (error){
+            if(axios.isAxiosError(error)){
+                console.error('Erro ao criar usuário: ',error.response?.data || error.message);
+                throw new Error(error.response?.data?.message || 'Erro ao criar usuário.');
+            } else {
+                console.error('Erro inesperado ao criar usuário:', error);
+                throw new Error('Ocorreu um erro inesperado.');
+            }
+            }
+        }
 }
+
